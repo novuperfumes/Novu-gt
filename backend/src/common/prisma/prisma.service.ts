5aca2bf719aa657f6de5fea8d7d07e1b6383be61
Samuel Mejía <150869146+Biggles-insano@users.noreply.github.com>
2026-08-18 13:23:09 -0600

    fix: restore mariadb adapter

--- backend/src/common/prisma/prisma.service.ts
+++ backend/src/common/prisma/prisma.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService
@@ -7,7 +8,9 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
+    const adapter = new PrismaMariaDb(process.env.DATABASE_URL || '');
     super({
+      adapter,
       log:
         process.env.NODE_ENV === 'development'
           ? ['query', 'info', 'warn', 'error']
